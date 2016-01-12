9d1132f568629c3cd36bab4ef51148744e728bd5
Prateek Lohani <prateek.lohani@sap.com>
2016-01-12 16:51:22 +0530

    code changed

--- Plugin/src/com/plugin/utils/Filereader.java
+++ Plugin/src/com/plugin/utils/Filereader.java
@@ -4,16 +4,26 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Filereader {
 
 	static String result;
 	private File file;
+	private int loc;
+	private int blankLines;
+	private int lineOfComments;
 	private StringBuilder filecontents;
 	private String currline;
 	private static Filereader fileobj = null;
 	private String filepath;
+	private Map<String, Integer> map = new HashMap<String, Integer>();
 	BufferedReader br = null;
+	
+	private final String KEY_FOR_LOC = "LOC";
+	private final String KEY_FOR_BLANK_LINES = "BL";
+	private final String KEY_FOR_COMMENTED_LINES = "CL";
 
 	public Filereader(String path) {// Constructor which takes File Path and
 									// calls "ReadContents" to read its contents
@@ -32,6 +42,7 @@ public class Filereader {
 
 			while ((currline = br.readLine()) != null) {
 				filecontents.append(currline + separator);
+
 			}
 
 		} catch (IOException e) {
@@ -47,8 +58,52 @@ public class Filereader {
 		return filecontents.toString();
 	}
 
-	public static void main(String args[]) {
-		fileobj = new Filereader("C:\\Users\\I320234\\Desktop\\mainactivity.txt");
+	/*
+	 * This method will return total no of lines of code, blank lines, commented
+	 * lines
+	 */
+	public Map<String, Integer> countLines() {
+		BufferedReader br = null;
+		boolean flag = false;
+		String line = null;
+		try {
+			br = new BufferedReader(new FileReader(filepath));
+			while ((line = br.readLine()) != null) {
+				
+				//This will count commented lines
+				if (line.contains("/*"))
+					flag = true;
+
+				if (flag)
+					lineOfComments++;
+
+				if (line.contains("*/"))
+					flag = false;
+
+				if (line.contains("//") && !flag)
+					lineOfComments++;
+				
+				//This part will count the blank lines
+				if (line.trim().isEmpty()) {
+					blankLines++;
+				}
+				
+				//This will count total no of lines
+				loc++;
+			}
+
+			map.put(KEY_FOR_LOC, loc);
+			map.put(KEY_FOR_BLANK_LINES, blankLines);
+			map.put(KEY_FOR_COMMENTED_LINES, lineOfComments);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return map;
 	}
+
+	// public static void main(String args[]) {
+	// fileobj = new
+	// Filereader("C:\\Users\\I320234\\Desktop\\mainactivity.txt");
+	// }
 
 }
